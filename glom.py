6bbd36d5db3673aa1b9d933ae07723bff343444c
Scott Mackie <mr.jsmackie@gmail.com>
2024-07-01 16:51:39 -0700

    update paradigm code to handle tables and multi-columns

--- glom.py
+++ glom.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 import argparse
 from fpdf import FPDF
 from ursus.rules import Rule
@@ -40,7 +41,7 @@ def align_glosses(top_line, morpheme_breakdown, glosses, translation, number=0):
 
     return '\n'.join([top_line, morphemes, gloss, translation, '\n']) #return extra newline for nice formatting
 
-def read_dictionary_files(cwd, dictionary_dir ):
+def read_dictionary_files(cwd, dictionary_dir, dictionary_order):
     lexicon = dict()
     path = os.path.join(cwd, dictionary_dir)
     for file in os.listdir(path):
@@ -50,41 +51,67 @@ def read_dictionary_files(cwd, dictionary_dir ):
         for line in lines:
             if not line:
                 continue
-            gloss,translation = line.strip().replace('-','').split(sep)[:2] #ignore any other data for now
-            # if translation_first:
-            #     gloss,translation = translation,gloss
-            lexicon[gloss.replace('-','')] = translation
+            gloss,morpheme = line.strip().replace('-','').split(sep)[:2] #ignore any other data for now
+            if dictionary_order.startswith('morph'):
+                 gloss,morpheme = morpheme,gloss
+            lexicon[gloss] = morpheme
     return lexicon
 
-def read_paradigm_files(cwd, paradigm_dir):
+def read_paradigm_files(cwd, paradigm_dir, table_order):
     paradigms = dict()
     paradigm_dir = os.path.join(cwd, paradigm_dir)
     for file in os.listdir(os.path.join(cwd, paradigm_dir)):
         with open(os.path.join(cwd, paradigm_dir, file), encoding='utf-8') as f:
-            lines = [line for line in f]
-            sep = ',' if len(lines[0].split(','))>1 else '\t'
-        if lines[0].startswith(sep):
-            columns = lines[0].strip().split(sep)[1:]  # first column 'header' is always empty
-        else:
-            columns = ['']
-        #rows = [line.strip().split(sep) for line in lines[1:]]
-        rows = list()
-        for line in lines[1:]:
-            if '#' in line:
-                line, comment = line.split('#')
-            row = line.strip().split(sep)
-            rows.append(row)
-        gloss = file.split('.')[0]
-        paradigms[gloss] = dict()
-        for row in rows:
-            paradigms[gloss][row[0]] = dict()
-            for j, col in enumerate(columns):
-                morpheme = row[j + 1]  # offset because first column is empty
-                if not col:
-                    paradigms[gloss][row[0]] = morpheme
-                else:
-                    paradigms[gloss][row[0]][col] = morpheme
+            data = f.read()
+        if '#' in data:
+            data,comments = data.strip().split('#')
+        sep = ',' if len(data[0].split(',')) > 1 else '\t'
+        blocks = re.split(r'\n\s*\n', data.strip())
+        for block in blocks:
+            block = block.strip()
+            if not block:
+                continue
+            rows = [b.split(',') for b in block.split('\n')]
+            second_row = rows[1]
+
+            if len([x for x in second_row if x.isupper()])>2:
+                #if there are at least two upper-case glosses in the second row, it's not a 2-dimenational table
+                #d = {}
+                for line in block.strip().split("\n"):
+                    parts = line.split(",")
+                    *keys, value = parts
+                    current_level = paradigms
+
+                    for key in keys[:-1]: #ignore final value, it's the transcription
+                        if key not in current_level:
+                            current_level[key] = {}
+                        current_level = current_level[key]
+
+                    # Set the final value
+                    current_level[keys[-1]] = value
+
+            else:
+                #read this as a classic 2D table where the top left element is the starting point of the gloss
+                headers, rows = rows[0], rows[1:]
+                gloss, columns = headers[0], headers[1:]
+                if table_order.startswith('col'):
+                    rows,columns = columns,rows
+                paradigms[gloss] = dict()
+                for row in rows:
+                    row_header, row_data = row[0], row[1:]
+                    paradigms[gloss][row_header] = dict()
+                    for j, col in enumerate(columns):
+                        morpheme = row_data[j]
+                        paradigms[gloss][row_header][col] = morpheme
+
     return paradigms
+
+def paradigm_lookup(sequence, dictionary):
+    keys = sequence.split(".")
+    result = dictionary
+    for key in keys:
+        result = result[key]
+    return result
 
 def get_morphemes(glosses, lexicon, paradigms):
     output = list()
@@ -98,32 +125,47 @@ def get_morphemes(glosses, lexicon, paradigms):
                     lexicon, result, errors = query_lexicon(lexicon, morpheme, errors)
                     new_word.append(result)
                 elif morpheme.isupper():
-                    if '.' in morpheme:
-                        morphs = morpheme.split('.')
-                        paradigm = morphs[0]
-                        row = morphs[1]
-                        try:
-                            col = morphs[2]
-                        except IndexError:
-                            col = None
-                        if col is not None:
-                            new_word.append(paradigms[paradigm][row][col])
+                    try:
+                        if '.' in morpheme:
+                            morph = paradigm_lookup(morpheme, paradigms)
+                            new_word.append(morph)
+                            # morphs = morpheme.split('.')
+                            # paradigm = morphs[0]
+                            # row = morphs[1]
+                            # try:
+                            #     col = morphs[2]
+                            # except IndexError:
+                            #     col = None
+                            # if col is not None:
+                            #     new_word.append(paradigms[paradigm][row][col])
+                            # else:
+                            #     new_word.append(paradigms[paradigm][row])
                         else:
-                            new_word.append(paradigms[paradigm][row])
-                    else:
-                        lexicon, result, errors = query_lexicon(lexicon, morpheme, errors)
-                        new_word.append(result)
+                            lexicon, result, errors = query_lexicon(lexicon, morpheme, errors)
+                            new_word.append(result)
+                    except KeyError as e:
+                        if not e:
+                            continue
+                        print(f'Something went wrong trying to read your input file.')
+                        print(f'While reading this gloss:{word}')
+                        print(f'GLOM encountered an error on this particular item:{e}')
+                        print('One of the following things probably happened:')
+                        print('- Simple spelling mistake. Maybe you typed INFL instead of INF, or 1POSS instead of 1.POSS')
+                        print('- Inconsistent glossing. Maybe you used "SG" for singular in one place and "SING" in another')
+                        print('- One of your paradigm files might have an extra comma or tab at the end of a line. Delete these.')
+                        print('- You might have reversed the order of information in a table. GLOM reads rows before columns by default, run with "--table_order column" if you prefer the columns first.')
+                        print('- You might have forgotten an element in your gloss. If your nominative case inflects for number and gender, make sure they are both included.')
+                        quit()
                 else:
                     lexicon, result, errors = query_lexicon(lexicon, morpheme, errors)
                     new_word.append(result)
-
             new_word = '-'.join(new_word)
             new_sentence.append(new_word)
         new_sentence = ' '.join(new_sentence)
         output.append(new_sentence)
     return output, errors
 
-def read_input_file(cwd, input_file, translation_first):
+def read_input_file(cwd, input_file, input_order):
     with open(os.path.join(cwd, input_file), encoding='utf-8') as f:
         lines = [line.strip() for line in f]
     glosses = list()
@@ -140,7 +182,7 @@ def read_input_file(cwd, input_file, translation_first):
             translation = ''
         else:
             gloss,translation = line
-            if translation_first:
+            if input_order == 'translation':
                  gloss,translation = translation,gloss
         glosses.append(gloss)
         translations.append(translation)
@@ -151,6 +193,8 @@ def generate_output_file(file_format, output_file, top_lines, morpheme_breakdown
     example = str()
     sentence_number = 0
     if file_format == 'text':
+        if not output_file.endswith('.txt'):
+            output_file += '.txt'
         with open(output_file, mode='w', encoding='utf-8') as f:
             for j in range(len(glosses)):
                 if add_sentence_numbers:
@@ -158,6 +202,8 @@ def generate_output_file(file_format, output_file, top_lines, morpheme_breakdown
                 example = align_glosses(top_lines[j], morpheme_breakdowns[j], glosses[j], translations[j], sentence_number)
                 print(example, file=f)
     elif file_format == 'pdf':
+        if not output_file.endswith('.pdf'):
+            output_file += '.pdf'
         pdf = FPDF()
         pdf.add_page()
         pdf.set_font("Arial", size=12)
@@ -194,21 +240,21 @@ def apply_sound_changes(examples, cwd, sound_change_file):
         output.append(' '.join(results))
     return output
 
-
-
 def construct_examples(input_file,
                       output_file,
                       dictionary_dir='dictionaries',
                       paradigm_dir='paradigms',
                       add_sentence_numbers=False,
                       file_format='text',
-                      translation_first=False,
+                      input_order ='translation',
+                      dictionary_order = 'gloss',
                       sound_change_file=None,
-                      print_one=False):
+                      print_one=False,
+                      table_order='rows'):
     cwd = os.getcwd()
-    lexicon = read_dictionary_files(cwd, dictionary_dir)
-    paradigms = read_paradigm_files(cwd, paradigm_dir)
-    glosses, morphemes = read_input_file(cwd, input_file, translation_first)
+    lexicon = read_dictionary_files(cwd, dictionary_dir, dictionary_order)
+    paradigms = read_paradigm_files(cwd, paradigm_dir, table_order)
+    glosses, morphemes = read_input_file(cwd, input_file, input_order)
     morpheme_breakdowns, errors = get_morphemes(glosses, lexicon, paradigms)
     top_lines = apply_sound_changes(morpheme_breakdowns, cwd, sound_change_file)
     example_sentence = generate_output_file(file_format, output_file, top_lines, morpheme_breakdowns, glosses, morphemes, add_sentence_numbers)
@@ -239,9 +285,11 @@ if __name__ == '__main__':
         parser.add_argument('-p', '--paradigm_dir', dest='paradigm_dir', default='paradigms', help='(Optional) Name of local directory containing paradigm files', required=False)
         parser.add_argument('-f', '--file_format', dest='file_format', default='text', required=False, help='(Optional) Choose format for the output file, options are text or pdf, defaults to text')
         parser.add_argument('-n', '--numbered_examples', dest='add_sentence_numbers', default=False, action='store_true', required=False, help='(Optional) Use this flag to add a number to each output sentence')
-        parser.add_argument('-tf', '--translation_first', dest='translation_first', default=False, action='store_true', required=False, help='(Optional) Indicates that dictionary files are formatted as translation then gloss')
+        parser.add_argument('-io', '--input_order', dest='input_order', default='translation', required=False, help='(Optional) Input files are ordered translation-first by default. Set this argument to "gloss" if you want the revered order')
+        parser.add_argument('-do', '--dictionary-order', dest='dictionary_order', default='gloss', required=False, help='(Optional) Dictionary files are ordered gloss-first by default. Set this argument to "morpheme" if you want the reversed order')
         parser.add_argument('-sc', '--sound_changes', dest='sound_change_file', default=False, required=False, help='(Optional) Name of local file containing sound change rules. See GLOM documentation for more details.')
-        parser.add_argument('-p1', '--print_one', dest='print_one', default=False, action='store_true', required=False, help='(Optional) include this flag if you want to see one example sentence printec in the console when GLOM is done.')
+        parser.add_argument('-p1', '--print_one', dest='print_one', default=False, action='store_true', required=False, help='(Optional) include this flag if you want to see one example sentence printed in the console when GLOM is done.')
+        parser.add_argument('-to', '--table_order', dest='table_order', default='row', required=False, help='(Optional) Paradigm tables are read in the order of rows then columns. Set this argument to "columns" if you want the reversed order')
         args = parser.parse_args()
         construct_examples(**vars(args))
 
